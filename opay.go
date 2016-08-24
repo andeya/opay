bb3e009641a368cf65109a190897f9d942332f6a
HenryLee <henrylee_cn@foxmail.com>
2016-08-24 20:19:46 +0800

    update

--- opay.go
+++ opay.go
@@ -4,13 +4,6 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-type Engine struct {
-	*AccList           //账户操作接口列表
-	*ServeMux          //订单操作接口全局路由
-	Queue              //订单队列接口
-	db        *sqlx.DB //全局数据库操作对象
-}
-
 // 新建订单处理服务
 func NewOpay(db *sqlx.DB, queueCapacity int) *Engine {
 	return &Engine{
@@ -19,6 +12,14 @@ func NewOpay(db *sqlx.DB, queueCapacity int) *Engine {
 		Queue:    newOrderChan(queueCapacity),
 		db:       db,
 	}
+}
+
+// 订单处理引擎
+type Engine struct {
+	*AccList           //账户操作接口列表
+	*ServeMux          //订单操作接口全局路由
+	Queue              //订单队列接口
+	db        *sqlx.DB //全局数据库操作对象
 }
 
 // 启动订单处理服务
